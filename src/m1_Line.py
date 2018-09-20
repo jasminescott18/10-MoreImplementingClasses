21fc0e69e8f42895aef5a047420f4700010454c7
scottjm1 <jasminescott2018@gmail.com>
2018-09-20 15:53:59 -0400

    Module 1

--- src/m1_Line.py
+++ src/m1_Line.py
@@ -3,8 +3,8 @@ A simple   Line   class.
 NOTE: This is NOT rosegraphics -- it is your OWN Line class.
 
 Authors: David Mutchler, Vibha Alangar, Dave Fisher, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Jasmine Scott.
+"""  # COMPLETED: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import math
 import m1t_test_Line as m1t
@@ -15,7 +15,7 @@ import m1t_test_Line as m1t
 ########################################################################
 
 # ----------------------------------------------------------------------
-# TODO: 2. With your instructor, READ THE INSTRUCTIONS
+# COMPLETED: 2. With your instructor, READ THE INSTRUCTIONS
 #   in file  m0_INSTRUCTIONS.txt, asking questions as needed.
 #   Once you understand the instructions, mark this TO DO as DONE.
 #
@@ -216,6 +216,10 @@ class Line(object):
           :type start: Point
           :type end:   Point
         """
+
+        self.start = start.clone()
+        self.end = end.clone()
+
         # --------------------------------------------------------------
         # TODO: 3.
         #   a. READ the above specification, including the Example.
@@ -325,6 +329,7 @@ class Line(object):
         Type hints:
           :rtype: Line
         """
+        return 
         # --------------------------------------------------------------
         # TODO: 4.
         #   a. READ the above specification, including the Example.
